674209bc732b61db6c56d34165c5fddbe8fea7e0
Roope Korkee <roope.korkee@tuni.fi>
2021-04-27 15:37:02 +0300

    Add new players to the playerData file

--- src/main/java/me/roopekoo/deathcounter/DeathHandler.java
+++ src/main/java/me/roopekoo/deathcounter/DeathHandler.java
@@ -1,15 +1,28 @@
 package me.roopekoo.deathcounter;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class DeathHandler implements Listener {
 
     @EventHandler
     public void on(PlayerDeathEvent e)
     {
-        DeathCounter.getPlugin().get_config().add(e);
+        Player player = e.getEntity();
+        DeathCounter.getPlugin().get_config().add(player);
     }
 
+    @EventHandler
+    public void on(PlayerJoinEvent e)
+    {
+        Player p = e.getPlayer();
+        if(p.hasPlayedBefore())
+        {
+            Player player = e.getPlayer();
+            DeathCounter.getPlugin().get_config().add(player);
+        }
+    }
 }
