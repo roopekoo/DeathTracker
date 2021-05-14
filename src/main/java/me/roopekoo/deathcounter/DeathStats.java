19584e46022cd3e4d0d130d5a0e6d7b4ffd0a5bf
Roope Korkee <roope.korkee@tuni.fi>
2021-05-14 15:12:55 +0300

    Add new statistics command

---
+++ src/main/java/me/roopekoo/deathcounter/DeathStats.java
@@ -0,0 +1,19 @@
+package me.roopekoo.deathcounter;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class DeathStats implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission("deathcounter.getstats")) {
+
+        }
+        else
+        {
+            sender.sendMessage("You don't have permission to do that!");
+        }
+        return true;
+    }
+}
