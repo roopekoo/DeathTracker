ab76e16d0824d94509c8090e4dbc62ce9377c179
Roope Korkee <roope.korkee@tuni.fi>
2021-05-15 00:37:46 +0300

    Joining and leaving updates playtime

--- src/main/java/me/roopekoo/deathcounter/DeathHandler.java
+++ src/main/java/me/roopekoo/deathcounter/DeathHandler.java
@@ -5,24 +5,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class DeathHandler implements Listener {
 
     @EventHandler
-    public void on(PlayerDeathEvent e)
-    {
+    public void on(PlayerDeathEvent e) {
         Player player = e.getEntity();
-        DeathCounter.getPlugin().get_file().add(player);
+        DeathCounter.getPlugin().get_file().addDeath(player);
     }
 
     @EventHandler
-    public void on(PlayerJoinEvent e)
-    {
+    public void on(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        if(p.hasPlayedBefore())
-        {
-            Player player = e.getPlayer();
-            DeathCounter.getPlugin().get_file().add(player);
+        DeathData d = DeathCounter.getPlugin().get_file();
+        if (!p.hasPlayedBefore()) {
+            d.addNewPlayer(p);
+        } else {
+            d.updateTime(p);
         }
+    }
+    @EventHandler
+    public void on(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        DeathCounter.getPlugin().get_file().updateTime(p);
     }
 }
