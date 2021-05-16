40fb4c936325162e2163e5bca9e8f0cb93e4ad1e
Roope Korkee <roope.korkee@tuni.fi>
2021-05-16 17:14:28 +0300

    Expand imports

--- src/main/java/me/roopekoo/deathtracker/DeathData.java
+++ src/main/java/me/roopekoo/deathtracker/DeathData.java
@@ -9,7 +9,9 @@ import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ForkJoinPool;
 
 public class DeathData
