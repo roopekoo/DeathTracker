d687b200634cda7af80fb8477267b9937c966e00
Roope Korkee <roope.korkee@tuni.fi>
2021-05-18 00:14:01 +0300

    Implement the whole command

--- src/main/java/me/roopekoo/deathtracker/DeathStats.java
+++ src/main/java/me/roopekoo/deathtracker/DeathStats.java
@@ -4,18 +4,56 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class DeathStats implements CommandExecutor
-{
-	@Override public boolean onCommand(CommandSender sender, Command command,
-	                                   String label, String[] args)
-	{
-		if(sender.hasPermission("deathtracker.getstats"))
-		{
-			//TODO: Do something here
-		}
-		else
-		{
-			sender.sendMessage("You don't have permission to do that!");
+public class DeathStats implements CommandExecutor {
+	@Override public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender.hasPermission("deathtracker.getstats")) {
+			if (args.length > 0) {
+				DeathData deathData = DeathTracker.getPlugin().get_file();
+				if (args.length == 2) {
+					if (args[0].equals("deaths")) {
+						if (args[1].equals("high")) {
+							sender.sendMessage(Lang.TITLE.toString() + Lang.HIGH_DEATHS);
+							deathData.printHighDeaths(sender);
+						} else if (args[1].equals("low")) {
+							sender.sendMessage(Lang.TITLE.toString() + Lang.LOW_DEATHS);
+							deathData.printLowDeaths(sender);
+						} else {
+							sender.sendMessage(Lang.TITLE.toString() + Lang.INVALID_PARAM);
+							return false;
+						}
+					} else if (args[0].equals("deathrate")) {
+						if (args[1].equals("high")) {
+							sender.sendMessage(Lang.TITLE.toString() + Lang.HIGH_DEATHRATE_TITLE);
+							deathData.printHighDeathRate(sender);
+						} else if (args[1].equals("low")) {
+							sender.sendMessage(Lang.TITLE.toString() + Lang.LOW_DEATHRATE_TITLE);
+							deathData.printLowDeathRate(sender);
+						} else {
+							sender.sendMessage(Lang.TITLE.toString() + Lang.INVALID_PARAM);
+							return false;
+						}
+					} else {
+						sender.sendMessage(Lang.TITLE.toString() + Lang.TOO_MANY_PARAMS);
+						return false;
+					}
+				} else if (args.length == 1) {
+					if (args[0].equals("immortals")) {
+						sender.sendMessage(Lang.TITLE.toString() + Lang.IMMORTAL_TITLE);
+						deathData.printImmortal(sender);
+					} else {
+						sender.sendMessage(Lang.TITLE.toString() + Lang.NOT_ENOUGH_PARAMS);
+						return false;
+					}
+				} else {
+					sender.sendMessage(Lang.TITLE.toString() + Lang.TOO_MANY_PARAMS);
+					return false;
+				}
+			} else {
+				sender.sendMessage(Lang.TITLE.toString() + Lang.NOT_ENOUGH_PARAMS);
+				return false;
+			}
+		} else {
+			sender.sendMessage(Lang.TITLE.toString() + Lang.NO_PERM);
 		}
 		return true;
 	}
