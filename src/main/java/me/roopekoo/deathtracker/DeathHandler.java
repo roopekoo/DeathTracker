923947044ac39f46b6d02c9b53146cfd1ddac3f9
Roope Korkee <roope.korkee@tuni.fi>
2021-05-19 14:10:53 +0300

    add DeathData method and edit updateTime

--- src/main/java/me/roopekoo/deathtracker/DeathHandler.java
+++ src/main/java/me/roopekoo/deathtracker/DeathHandler.java
@@ -1,5 +1,6 @@
 package me.roopekoo.deathtracker;
 
+import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,24 +8,28 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+
 public class DeathHandler implements Listener {
+	private final DeathData deathData = DeathTracker.getPlugin().get_file();
+
 	@EventHandler public void on(PlayerDeathEvent e) {
 		Player player = e.getEntity();
-		DeathTracker.getPlugin().get_file().addDeath(player);
+		deathData.addDeath(player);
 	}
 
 	@EventHandler public void on(PlayerJoinEvent e) {
 		Player p = e.getPlayer();
-		DeathData d = DeathTracker.getPlugin().get_file();
 		if(!p.hasPlayedBefore()) {
-			d.addNewPlayer(p);
+			deathData.addNewPlayer(p);
 		} else {
-			d.updateTime(p);
+			int totalPlayTime = p.getStatistic(Statistic.PLAY_ONE_MINUTE);
+			deathData.updateTime(p.getUniqueId().toString(),totalPlayTime);
 		}
 	}
 
 	@EventHandler public void on(PlayerQuitEvent e) {
 		Player p = e.getPlayer();
-		DeathTracker.getPlugin().get_file().updateTime(p);
+		int totalPlayTime = p.getStatistic(Statistic.PLAY_ONE_MINUTE);
+		deathData.updateTime(p.getUniqueId().toString(),totalPlayTime);
 	}
 }
