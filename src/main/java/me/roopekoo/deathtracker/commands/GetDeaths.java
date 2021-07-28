e43057deffbaca97acfeff189571a3c061d4b2ec
Roope Korkee <roope.korkee@tuni.fi>
2021-07-28 18:54:37 +0300

    Add /getdeaths total <time|deaths> command

--- src/main/java/me/roopekoo/deathtracker/commands/GetDeaths.java
+++ src/main/java/me/roopekoo/deathtracker/commands/GetDeaths.java
@@ -10,6 +10,9 @@ import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 public class GetDeaths implements CommandExecutor {
 	private final DeathTimeConverter converter = new DeathTimeConverter();
@@ -17,17 +20,81 @@ public class GetDeaths implements CommandExecutor {
 	@Override public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if(sender.hasPermission("deathtracker.getdeaths")) {
 			if(args.length>0) {
-				String name = args[0];
-				OfflinePlayer pl = Bukkit.getOfflinePlayer(name);
-				if(!pl.hasPlayedBefore()) {
-					sender.sendMessage(Lang.TITLE.toString()+Lang.INVALID_PLAYER);
-					return true;
-				}
-				name = pl.getName();
-				assert name != null;
-				DeathData deathData = DeathTracker.getPlugin().get_file();
-				int totalPlayTime = pl.getStatistic(Statistic.PLAY_ONE_MINUTE);
-				deathData.updateTime(pl.getUniqueId().toString(), totalPlayTime, false);
+				if(args[0].equals("total")) {
+					if(args.length == 1) {
+						sender.sendMessage(Lang.TITLE.toString()+Lang.NOT_ENOUGH_PARAMS);
+						return false;
+					} else if(args.length == 2) {
+						DeathData dd = DeathTracker.getPlugin().get_file();
+						if(args[1].equals("deaths")) {
+							//TITLE
+							String msg = Lang.TITLE.toString()+Lang.TOT_DEATHS_TITLE;
+							String totPl = String.valueOf(dd.getTotalActPlayer());
+							String plText = Lang.PLAYERS.toString();
+							if(totPl.equals("1")) {
+								plText = Lang.PLAYER.toString();
+							}
+							String s = combineTotalTitle(msg, totPl, plText);
+							sender.sendMessage(s);
+
+							//MAIN MESSAGE
+							msg = Lang.TOT_DEATH_STATS.toString();
+							String totDeaths = String.valueOf(dd.getTotalDeaths());
+							String deathText = Lang.DEATHS.toString();
+							if(totDeaths.equals("1")) {
+								deathText = Lang.DEATH.toString();
+							}
+
+							String totPlayTime = converter.playTicksToShortStr(dd.getTotalPlaytime());
+							s = combineDeathsSTR(msg, "", totDeaths, deathText, totPlayTime);
+							sender.sendMessage(s);
+						} else if(args[1].equals("time")) {
+							//TITLE
+							String msg = Lang.TITLE.toString()+Lang.TOT_DEATHRATE_TITLE;
+							String totPl = String.valueOf(dd.getTotalActPlayer());
+							String plText = Lang.PLAYERS.toString();
+							if(totPl.equals("1")) {
+								plText = Lang.PLAYER.toString();
+							}
+							String s = combineTotalTitle(msg, totPl, plText);
+							sender.sendMessage(s);
+
+							//MAIN MESSAGE
+
+							double deathValue = dd.getTotalDeaths();
+							double playtime = dd.getTotalPlaytime();
+							double deathTime = 0;
+							double timeDeath = 0;
+							if(playtime != 0) {
+								deathTime = deathValue/playtime;
+							}
+							if(deathValue != 0) {
+								timeDeath = playtime/deathValue;
+							}
+							String deathPerTime = converter.deathPerTime(deathTime);
+							String timePerDeath = converter.playTicksToShortStr(timeDeath);
+
+							msg = Lang.TOT_DEATHRATE_STATS.toString();
+							s = combineTimeSTR(msg, "", deathPerTime, timePerDeath);
+							sender.sendMessage(s);
+						} else {
+							sender.sendMessage(Lang.TITLE.toString()+Lang.INVALID_PARAM);
+						}
+					} else {
+						sender.sendMessage(Lang.TITLE.toString()+Lang.TOO_MANY_PARAMS);
+					}
+				} else {
+					String name = args[0];
+					OfflinePlayer pl = Bukkit.getOfflinePlayer(name);
+					if(!pl.hasPlayedBefore()) {
+						sender.sendMessage(Lang.TITLE.toString()+Lang.INVALID_PLAYER);
+						return true;
+					}
+					name = pl.getName();
+					assert name != null;
+					DeathData deathData = DeathTracker.getPlugin().get_file();
+					int totalPlayTime = pl.getStatistic(Statistic.PLAY_ONE_MINUTE);
+					deathData.updateTime(pl.getUniqueId().toString(), totalPlayTime, false);
 
 				double deathValue = deathData.getDeaths(pl.getUniqueId());
 				String deathText = Lang.DEATHS.toString();
